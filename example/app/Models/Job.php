d22f75310d0c2d55284635517d7347e7c6b129ac
shehzadkhalid87 <shehzadkhalid04@gmail.com>
2025-10-17 04:20:40 +0500

    worked on Eloquent

--- example/app/Models/Job.php
+++ example/app/Models/Job.php
@@ -3,13 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class Job extends Model
 {
+    use HasFactory;
+
     protected $table = 'job_listing';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+
+    }
 
 
 }
---
+++ example/database/factories/JobFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class JobFactory extends Factory
+{
+
+
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle(),
+            'employer_id' => Employer::factory(),
+            'salary' => '$60,000 USD'
+        ];
+        // TODO: Implement definition() method.
+    }
+}
